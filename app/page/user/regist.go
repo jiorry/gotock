761caecf84d9459488aa59946b7c10d8b6550a7d
jiorry <jiorry@hotmail.com>
2015-09-04 14:42:46 +0800

    a

--- app/page/user/regist.go
+++ app/page/user/regist.go
@@ -10,14 +10,15 @@ type Regist struct {
 	gos.Page
 }
 
-func (p *Regist) Befor() bool {
-	p.View.Folder = "user"
-	p.Cache.Type = gos.PAGE_CACHE_FILE
-	return true
-}
+// func (p *Regist) Befor() bool {
+// 	p.View.Folder = "user"
+// 	p.Cache.Type = gos.PAGE_CACHE_FILE
+// 	return true
+// }
 
 func (p *Regist) Prepare() bool {
 	p.Title = "用户注册"
+	p.View.Folder = "user"
 	common.SetupPage(&p.Page, "default")
 
 	p.Layout.TopRenderList = nil

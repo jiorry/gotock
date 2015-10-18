aab4344e8308c42d0330879a9bd372a92934e57f
jiorry <jiorry@hotmail.com>
2015-10-19 01:09:53 +0800

    a

--- webroot/assets/js/dev/page/home/Default.js
+++ webroot/assets/js/dev/page/home/Default.js
@@ -19,94 +19,86 @@ require(
 
 		var h,l,v
 
-		var $ph = $('#inputPh'),
-			$pl = $('#inputPl'),
-			$pv = $('#inputPv'),
-			$typ = $('#selectType');
+		var $pA = $('#inputA'),
+			$pB = $('#inputB'),
+			$pV = $('#inputV'),
+			$typ = $('#selectType'),
+			$inputPA = $('#inputPA'),
+			$inputPB = $('#inputPB');
 
+		$inputPA.change(function(){
+			$inputPB.val(1-parseFloat($inputPA.val()));
+		})
+		$inputPB.change(function(){
+			$inputPA.val(1-parseFloat($inputPB.val()));
+		})
 		function setLabel(){
 			var power = parseFloat($typ.val());
-			$('#labelHeigh').text('^'+(1-power).toString());
-			$('#labelLow').text('^'+power.toString());
+			$inputPA.val(1-power);
+			$inputPB.val(power);
 		}
 		setLabel();
 
 		$typ.change(function(){
-			$ph.trigger('focusout');
+			$pA.trigger('focusout');
 			setLabel();
 		})
-		$ph.focusout(function(){
-			h = parseFloat($ph.val());
-			l = parseFloat($pl.val());
-			v = parseFloat($pv.val());
+		$pA.focusout(function(){
+			h = parseFloat($pA.val());
+			l = parseFloat($pB.val());
+			v = parseFloat($pV.val());
 			if(isNaN(h) ||h==0){
 				return
 			}
 
 			if(h>0&&l>0&&v>0){
-				$pv.val(0);
+				$pV.val(0);
 			}
 			calculate();
 		})
-		$pl.focusout(function(){
-			h = parseFloat($ph.val());
-			l = parseFloat($pl.val());
-			v = parseFloat($pv.val());
+		$pB.focusout(function(){
+			h = parseFloat($pA.val());
+			l = parseFloat($pB.val());
+			v = parseFloat($pV.val());
 			if(isNaN(l) ||l==0){
 				return
 			}
 
 			if(h>0&&l>0&&v>0){
-				$pv.val(0);
+				$pV.val(0);
 			}
 			calculate();
 		})
-		$pv.focusout(function(){
-			h = parseFloat($ph.val());
-			l = parseFloat($pl.val());
-			v = parseFloat($pv.val());
+		$pV.focusout(function(){
+			h = parseFloat($pA.val());
+			l = parseFloat($pB.val());
+			v = parseFloat($pV.val());
 			if(isNaN(v) || v==0){
 				return
 			}
 
 			if(h>0&&l>0&&v>0){
-				$pl.val(0);
+				$pB.val(0);
 			}
 			calculate();
 		})
 
 		function calculate(){
 			var power = parseFloat($typ.val());
-			h = parseFloat($ph.val());
-			l = parseFloat($pl.val());
-			v = parseFloat($pv.val());
+			h = parseFloat($pA.val());
+			l = parseFloat($pB.val());
+			v = parseFloat($pV.val());
 
 			if(h>0 && l>0){
 				v = Math.pow(h, 1-power) * Math.pow(l, power);
-				$pv.val(v.toFixed(2));
+				$pV.val(v.toFixed(2));
 			}else if(l>0 && v>0){ // l>0 && v>0
 			   h = Math.pow(v/Math.pow(l, power), 1/(1-power))
-			   $ph.val(h.toFixed(2));
+			   $pA.val(h.toFixed(2));
 		   	}else if(h>0 && v>0){
 			   l = Math.pow(v/Math.pow(h, 1-power), 1/power)
-			   $pl.val(l.toFixed(2));
+			   $pB.val(l.toFixed(2));
 		   	}
 		}
-		// function calculate(){
-		// 	h = parseFloat($ph.val());
-		// 	l = parseFloat($pl.val());
-		// 	v = parseFloat($pv.val());
-		//
-		// 	if(h>0 && l>0){
-		// 		v = Math.pow(h, 0.382) * Math.pow(l, 0.618);
-		// 		$pv.val(v.toFixed(2));
-		// 	}else if(l>0 && v>0){ // l>0 && v>0
-		// 	   	h = Math.pow(v/Math.pow(l, 0.618), 1/0.382)
-		// 	   	$ph.val(h.toFixed(2));
-		//    	}else if(h>0 && v>0){
-		// 	   	l = Math.pow(v/Math.pow(h, 0.382), 1/0.618)
-		// 	   	$pl.val(l.toFixed(2));
-		//    	}
-		// }
 
 });
